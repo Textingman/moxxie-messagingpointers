c7c4482ee72e652c29bd9f8dbf2d9a62b2054cad
Textingman <matt.morf@textvolt.com>
2026-07-01 18:44:33 -0400

    Initial site setup: Messaging Pointers

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
           <div>
             <h4 className="font-semibold mb-4">Contact</h4>
             <p style={{ color: '#BAE6FD' }}>support@messagingpointers.com</p>
-            <p className="mt-2" style={{ color: '#BAE6FD' }}>8 The Green, Ste A, Dover, DE 19901</p>
+            <p className="mt-2" style={{ color: '#BAE6FD' }}>800 N King Street 304-1991, Wilmington, DE 19801</p>
             <p className="mt-2" style={{ color: '#BAE6FD' }}>(833) 648-2917</p>
           </div>
         </div>
